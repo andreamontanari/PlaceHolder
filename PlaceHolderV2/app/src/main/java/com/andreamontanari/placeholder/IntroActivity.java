2f184188c354927b0a72b927d6e8e76547f1d77e
Andrea Montanari <andrea.montanari92@gmail.com>
2016-06-27 23:24:40 +0200

    version2 project base

---
+++ PlaceHolderV2/app/src/main/java/com/andreamontanari/placeholder/IntroActivity.java
@@ -0,0 +1,18 @@
+package com.andreamontanari.placeholder;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by andreamontanari on 27/06/16.
+ */
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_intro);
+    }
+}
