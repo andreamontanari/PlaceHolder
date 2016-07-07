c6fd3bf71bc23177b5ca7486e3c081804c8829d3
Andrea Montanari <andrea.montanari92@gmail.com>
2016-07-08 01:19:41 +0200

    add map logic

--- PlaceHolderV2/app/src/main/java/com/andreamontanari/placeholder/IntroActivity.java
+++ PlaceHolderV2/app/src/main/java/com/andreamontanari/placeholder/IntroActivity.java
@@ -1,17 +1,40 @@
 package com.andreamontanari.placeholder;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.andreamontanari.placeholder.utils.GPSTracker;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 /**
  * Created by andreamontanari on 27/06/16.
  */
 public class IntroActivity extends AppCompatActivity {
+
+    /**
+     * Constant used in the location settings dialog.
+     */
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+
+    GPSTracker gps;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,8 +53,14 @@ public class IntroActivity extends AppCompatActivity {
     }
 
     public void savePlace(View view) {
-        Intent intent = new Intent(IntroActivity.this, MapsActivity.class);
-        startActivity(intent);
+
+        gps = new GPSTracker(this);
+        if(gps.canGetLocation()){
+            Intent intent = new Intent(IntroActivity.this, MapsActivity.class);
+            startActivity(intent);
+        }else {
+            displayLocationSettingsRequest(this);
+        }
     }
 
     private void setToolBar() {
@@ -39,5 +68,51 @@ public class IntroActivity extends AppCompatActivity {
         setSupportActionBar(tb);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(false);
+    }
+
+
+    private void displayLocationSettingsRequest(Context context) {
+        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(LocationServices.API).build();
+        googleApiClient.connect();
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(10000 / 2);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        //All location settings are satisfied
+                        Intent intent = new Intent(IntroActivity.this, MapsActivity.class);
+                        startActivity(intent);
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        //Location settings are not satisfied. Show the user a dialog to upgrade location settings
+
+                        try {
+                            // Show the dialog by calling startResolutionForResult(), and check the result
+                            // in onActivityResult().
+                            status.startResolutionForResult(IntroActivity.this, REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                            // PendingIntent unable to execute request
+                            Toast.makeText(IntroActivity.this, "ERROR, please try again", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        //Location settings are inadequate, and cannot be fixed here. Dialog not created
+                        Toast.makeText(IntroActivity.this, "ERROR, please try again", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
     }
 }
--- PlaceHolderV2/app/src/main/java/com/andreamontanari/placeholder/MapsActivity.java
+++ PlaceHolderV2/app/src/main/java/com/andreamontanari/placeholder/MapsActivity.java
@@ -5,14 +5,21 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.andreamontanari.placeholder.utils.GPSTracker;
+import com.github.clans.fab.FloatingActionButton;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -21,34 +28,43 @@ import com.google.android.gms.maps.model.MarkerOptions;
 /**
  * Created by andreamontanari on 07/07/16.
  */
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private GPSTracker gps;
+    private LatLng latlng;
+    private final int ZOOM_LEVEL = 15;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        
+
         setToolBar();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap) throws SecurityException {
         mMap = googleMap;
 
+        mMap.setMyLocationEnabled(true);
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        gps = new GPSTracker(this);
+        if(gps.canGetLocation()) {
+            //retrieve coordinates
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+            latlng = new LatLng(latitude, longitude);
+            //zoom in the map by level 16
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, ZOOM_LEVEL));
         }
     }
-
 
     private void setToolBar() {
         Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
@@ -66,6 +82,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 startActivity(homeIntent);
         }
         return (super.onOptionsItemSelected(menuItem));
+    }
+
+    public void addNewPlace(View v) {
+        gps = new GPSTracker(this);
+        if(gps.canGetLocation()) {
+            //retrieve coordinates
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+            latlng = new LatLng(latitude, longitude);
+            mMap.addMarker(new MarkerOptions()
+                    .position(latlng)
+                    .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_add_location_white_48dp))
+                    .title("Last saved place!"));
+        }
+
+        FloatingActionButton add_note_btn = (FloatingActionButton) findViewById(R.id.add_note_fab);
+        add_note_btn.setVisibility(View.VISIBLE);
+        Toast.makeText(this, "Registered place!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void addNotePlace(View v) {
+        FloatingActionButton add_note_btn = (FloatingActionButton) findViewById(R.id.add_note_fab);
+        add_note_btn.setVisibility(View.GONE);
+        mMap.clear();
+        Toast.makeText(this, "Registered note for shown place!", Toast.LENGTH_SHORT).show();
     }
 
 }
