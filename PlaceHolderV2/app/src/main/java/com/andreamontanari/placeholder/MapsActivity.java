37213f37b31dd67e0fcb5546226d94d968b0a4c6
Andrea Montanari <andrea.montanari92@gmail.com>
2016-07-07 15:04:16 +0200

    Added sample code

--- PlaceHolderV2/app/src/main/java/com/andreamontanari/placeholder/MapsActivity.java
+++ PlaceHolderV2/app/src/main/java/com/andreamontanari/placeholder/MapsActivity.java
@@ -12,18 +12,34 @@ import android.view.MenuItem;
 /**
  * Created by andreamontanari on 07/07/16.
  */
-public class MapsActivity extends AppCompatActivity {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_maps);
-
+        
         setToolBar();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
+    }
+
 
     private void setToolBar() {
         Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
