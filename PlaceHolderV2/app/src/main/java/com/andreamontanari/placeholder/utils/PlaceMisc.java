60453cb133ac4316e76b6cd721efc1ce7848143b
Andrea Montanari <andrea.montanari92@gmail.com>
2016-07-12 15:52:39 +0200

    uploaded get directions functionality

--- PlaceHolderV2/app/src/main/java/com/andreamontanari/placeholder/utils/PlaceMisc.java
+++ PlaceHolderV2/app/src/main/java/com/andreamontanari/placeholder/utils/PlaceMisc.java
@@ -95,5 +95,28 @@ public class PlaceMisc {
             }
         });
     }
+    
+    public void getDirections(double latitude, double longitude, Context context) {
+
+        String uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?daddr=%f,%f", latitude, longitude);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+        try
+        {
+            startActivity(intent);
+        }
+        catch(ActivityNotFoundException ex)
+        {
+            try
+            {
+                Intent unrestrictedIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                startActivity(unrestrictedIntent);
+            }
+            catch(ActivityNotFoundException innerEx)
+            {
+                Toast.makeText(context, "Please install a maps application", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 
 }
