bfd2fcb39a07b5cc155a9733411c14690147228b
Andrea Montanari <andrea.montanari92@gmail.com>
2016-07-19 23:12:24 +0200

    updated with twitter sharing option

--- PlaceHolderV2/app/src/main/java/com/andreamontanari/placeholder/PlacesActivity.java
+++ PlaceHolderV2/app/src/main/java/com/andreamontanari/placeholder/PlacesActivity.java
@@ -3,10 +3,13 @@ package com.andreamontanari.placeholder;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -24,6 +27,10 @@ import com.facebook.FacebookSdk;
 import com.andreamontanari.placeholder.utils.PlaceMisc;
 import com.facebook.share.model.ShareLinkContent;
 import com.facebook.share.widget.ShareDialog;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.List;
 
 /**
  * Created by andreamontanari on 07/07/16.
@@ -133,9 +140,45 @@ public class PlacesActivity extends AppCompatActivity {
                 }
                 break;
             case R.id.twitterChk:
-            Toast.makeText(this, "Coming soon", Toast.LENGTH_SHORT).show();
+                Intent tweetIntent = new Intent(Intent.ACTION_SEND);
+                tweetIntent.putExtra(Intent.EXTRA_TEXT, message + " #placeholder");
+                tweetIntent.setType("text/plain");
+
+                PackageManager packManager = getPackageManager();
+                List<ResolveInfo> resolvedInfoList = packManager.queryIntentActivities(tweetIntent,  PackageManager.MATCH_DEFAULT_ONLY);
+
+                boolean resolved = false;
+                for(ResolveInfo resolveInfo: resolvedInfoList){
+                    if(resolveInfo.activityInfo.packageName.startsWith("com.twitter.android")){
+                        tweetIntent.setClassName(
+                                resolveInfo.activityInfo.packageName,
+                                resolveInfo.activityInfo.name );
+                        resolved = true;
+                        break;
+                    }
+                }
+                if(resolved){
+                    startActivity(tweetIntent);
+                }else{
+                    Intent i = new Intent();
+                    i.putExtra(Intent.EXTRA_TEXT, message + " #placeholder");
+                    i.setAction(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse("https://twitter.com/intent/tweet?text="+urlEncode(message + " #placeholder")));
+                    startActivity(i);
+                    Toast.makeText(this, getResources().getString(R.string.twitter_not_installed), Toast.LENGTH_LONG).show();
+                }
+
                 break;
 
+        }
+    }
+
+    private String urlEncode(String s) {
+        try {
+            return URLEncoder.encode(s, "UTF-8");
+        }catch (UnsupportedEncodingException e) {
+            Log.wtf("ERROR", "UTF-8 should always be supported", e);
+            return "";
         }
     }
 }
