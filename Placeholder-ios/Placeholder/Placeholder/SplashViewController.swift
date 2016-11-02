50d92b36c9cd6ee73732e1e34821c82cce1f728e
Andrea Montanari <andrea.montanari92@gmail.com>
2016-11-03 00:49:29 +0100

    applied final fixes before last testing

---
+++ Placeholder-ios/Placeholder/Placeholder/SplashViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SplashViewController.swift
+//  Placeholder
+//
+//  Created by Andrea Montanari on 01/11/16.
+//  Copyright © 2016 Andrea Montanari. All rights reserved.
+//
+
+import Foundation
+
+//
+//  ViewController.swift
+//  Placeholder
+//
+//  Created by Andrea Montanari on 09/08/16.
+//  Copyright © 2016 Andrea Montanari. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import GoogleMaps
+
+class SplashViewController: UIViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
